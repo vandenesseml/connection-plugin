02e90dffa4e765000ff74196eaf23ea0bb7ddcd0
Max <vandenesseml@vcu.edu>
2019-05-16 09:17:29 -0400

    adjust path lines

--- src/utils.ts
+++ src/utils.ts
@@ -7,10 +7,10 @@ function toTrainCase(str: string) {
 
 export function defaultPath(points: number[], curvature: number) {
     const [x1, y1, x2, y2] = points;
-    const hx1 = x1 + Math.abs(x2 - x1) * curvature;
-    const hx2 = x2 - Math.abs(x2 - x1) * curvature;
+    const hx1 = Math.abs((x2 + x1) / 2);
+    const hx2 = Math.abs((x2 + x1) / 2);
 
-    return `M ${x1} ${y1} C ${hx1} ${y1} ${hx2} ${y2} ${x2} ${y2}`;
+    return `M ${x1} ${y1} L ${hx1} ${y1} ${hx2} ${y2} ${x2} ${y2}`;
 }
 
 export function renderPathData(emitter: Emitter<EventsTypes>, points: number[], connection?: Connection) {
